a82b7b1f8a82e9010dd2436ed734debe72d2381d
Krishan Gharu <kslg257@gmail.com>
2022-06-14 20:54:06 +0000

    updates

--- map.js
+++ map.js
@@ -6,14 +6,33 @@
 
 // Using a for loop
 
+  // To create a new array containing each of these numbers multiplied by 2.  
+let nums = [1, 2, 3, 4, 5];
+let results = []; // empty array
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
+
 
 
 // Simplified w/ map()
-
+const newArr = nums.map(myFunction)
+function myFunction(num) {
+  return num * 2;
+}
+console.log(newArr);
 
 // Simplfied w/ map() + arrow function
+let myFunc = nums.map(num => num * 2) ;
+console.log(myFunc);
 
 
 // With objects:
@@ -37,3 +56,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+const studentsWithIds = students.map(student => [student.name, student.id])
+console.log(studentsWithIds)
+  // output: [ [ 'Mark', 1 ], [ 'Ariel', 2 ], [ 'Jason', 3 ] ]
